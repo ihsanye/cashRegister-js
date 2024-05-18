9b5fd057fb06b3b37e5e3a405364ab5b89ed8f17
ihsanye <ihsany98@gmail.com>
2024-05-19 02:12:26 +0300

    0.01 problem :(

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ purchaseMoney.addEventListener("click", () => {
         "NICKEL": 0.05,
         "PENNY": 0.01
     };
-
     let cash = cashMoney.value;
     if (cash < price) {
         alert("Customer does not have enough money to purchase the item");
@@ -36,29 +35,38 @@ purchaseMoney.addEventListener("click", () => {
         changeMoney.innerText = "No change due - customer paid with exact cash";
     }
     else {
+        let changeArr = [];
+        let change = cash - price;
+        let totalCID = 0;
+        for (let i = 0; i < cid.length; i++) {
+            totalCID += cid[i][1] * 100;
+        }
+        totalCID /= 100;
+
         for (let i = cid.length - 1; i >= 0; i--) {
-            let currUnitsName = cid[i][0];
-            let currUnitsTotal = cid[i][1];
-            let coinAmount = Math.round(currUnitsTotal / currUnits[currUnitsName]);
-            console.log(coinAmount);
+            let currUnitsName = cid[i][0];//TWENTY
+            let currUnitsTotal = cid[i][1];//60
+            let currUnitsValue = currUnits[currUnitsName];//20
+            let coinAmount = Math.round(currUnitsTotal / currUnitsValue);//3
+            let howManyCoin = 0;
+
+            while (change >= currUnitsValue && coinAmount > 0) {
+                change -= currUnitsValue;
+                coinAmount--;
+                howManyCoin++;
+            }
+            if (howManyCoin != 0) {
+                changeArr.push([`${currUnitsName}: $${howManyCoin * currUnitsValue}`]);
+            }
+        }
+        if (totalCID < change) {
+            changeMoney.innerText = `Status: INSUFFICIENT_FUNDS`;
+        }
+        else if (totalCID == change) {
+            changeMoney.innerText = `Status: CLOSED ${changeArr}`.replaceAll(",", " ");
+        }
+        else if (totalCID > change) {
+            changeMoney.innerText = `Status: OPEN ${changeArr}`.replaceAll(",", " ");
         }
     }
 })
-
-const statusInfo = () => {
-    let change = 335.41;
-    let total = 0;
-    for (let i = 0; i < cid.length; i++) {
-        total += cid[i][1] * 100;
-    }
-    total /= 100;
-    if (total < change) {
-        return "Status: INSUFFICIENT_FUNDS";
-    }
-    else if (total == change) {
-        return "Status: CLOSED";
-    }
-    else if (total > change) {
-        return "Status: OPEN";
-    }
-}
